3e0695cbef54fe034519960d6d73d61cae41edc4
tdhood <taylordannhood@gmail.com>
2022-07-05 16:44:42 -0700

    more files

--- routes/shipments.js
+++ routes/shipments.js
@@ -5,6 +5,8 @@ const router = new express.Router();
 
 const { shipProduct } = require("../shipItApi");
 
+const shipmentsSchema = require("../shipmentsSchema.json");
+
 /** POST /ship
  *
  * VShips an order coming from json body:
@@ -14,10 +16,17 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
-  return res.json({ shipped: shipId });
+  const results = jsonschema.validate(req.body.shipments, shipmentsSchema, {
+    required: true,
+  });
+  if (!results.valid) {
+    const errs = result.errors.map((err) => err.stack);
+    throw new BadRequestError(errs);
+  } else {
+    const { productId, name, addr, zip } = req.body;
+    const shipId = await shipProduct({ productId, name, addr, zip });
+    return res.json({ shipped: shipId });
+  }
 });
-
 
 module.exports = router;
