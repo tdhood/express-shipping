762f35ffb656740a0a768056e1445eead6758252
tdhood <taylordannhood@gmail.com>
2022-07-05 17:11:37 -0700

    part one

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const {BadRequestError} = require("../expressError");
+const jsonschema = require("jsonschema");
 const express = require("express");
 const router = new express.Router();
 
@@ -16,11 +18,11 @@ const shipmentsSchema = require("../shipmentsSchema.json");
  */
 
 router.post("/", async function (req, res, next) {
-  const results = jsonschema.validate(req.body.shipments, shipmentsSchema, {
-    required: true,
+  const results = jsonschema.validate(req.body, shipmentsSchema, {
+    required: true
   });
   if (!results.valid) {
-    const errs = result.errors.map((err) => err.stack);
+    const errs = results.errors.map((err) => err.stack);
     throw new BadRequestError(errs);
   } else {
     const { productId, name, addr, zip } = req.body;
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -10,9 +10,30 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zip: "12345-6789",
+      zipcode: "12345-6789",
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+});
+
+
+describe("POST /", function () {
+  test("invalid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zipcode: 12345-6789,
+    });
+
+    expect(resp.body).toEqual({
+      "error": {
+        "message": [
+          "instance.zipcode is not of a type(s) string"
+        ],
+        "status": 400
+      }
+    });
   });
 });
